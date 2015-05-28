fb438877fa626d735cff59aa3166f9baf6334a37
Huy Nguyen <huynguyen.me@gmail.com>
2015-05-27 19:25:11 -0700

    Add Chart

--- js/main.js
+++ js/main.js
@@ -9,4 +9,25 @@ jQuery(document).ready(function($){
       event.preventDefault();
     }
   });
+
+
+  $('#indicator-select').selectize({
+      searchField: ['name', 'value'],
+      options: [
+        {name: "Access to electricity", value: "(% of population)"},
+        {name: "Adequacy of social insurance programs", value: "(% of total welfare of beneficiary households)"},
+        {name: "Adjusted savings: net forest depletion", value: "(current US$)"},
+        {name: "CO2 intensity", value: "(kg per kg of oil equivalent energy use)"},
+        {name: "Child employment in agriculture", value: "(% of economically active children ages 7-14)"},
+      ]
+  });
+
+  $('#country-select').selectize({
+    maxItems: 4,
+    items: [1,2],
+    plugins: ['remove_button']
+  });
+
+
+
 });
